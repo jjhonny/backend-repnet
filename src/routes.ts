441e445173cd25f4789169ea97de845c093308da
jjhonny <ojhonny1@hotmail.com>
2024-04-30 18:50:50 -0300

    feat: create route detail user

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
+import { DetailUserController } from "./controllers/user/DetailUserController";
+import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
 
 //-- ROTAS USER --
 router.post("/cadastro", new CreateUserController().handle);
 router.post("/login", new AuthUserController().handle);
+router.get("/me", isAuthenticated, new DetailUserController().handle);
 
 export { router };
