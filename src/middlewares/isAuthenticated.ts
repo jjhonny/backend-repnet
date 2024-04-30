50eaa890eb7d6980bd8c03c08d1571edff1dcbf7
jjhonny <ojhonny1@hotmail.com>
2024-04-30 18:56:40 -0300

    create type req.user_id

--- src/middlewares/isAuthenticated.ts
+++ src/middlewares/isAuthenticated.ts
@@ -22,6 +22,10 @@ export function isAuthenticated(
   try {
     // Validar esse token
     const { sub } = verify(token, process.env.JWT_SECRET) as PayLoad;
+
+    // Recuperar o id do token e colocar dentro de uma variavel user_id dentro do req
+    req.user_id = sub;
+
     return next();
   } catch (error) {
     return res.status(401).end();
